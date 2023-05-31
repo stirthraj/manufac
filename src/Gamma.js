887d943f7da629b64f2ef6d0abaa578a8f742b43
Tirth Raj Kumar <tirth.raj@bizztm.com>
2023-05-31 18:03:14 +0530

    median index change

--- src/Gamma.js
+++ src/Gamma.js
@@ -5,19 +5,18 @@ import Mode from "./components/mode"
 import { addingKey, classWiseGroup, modeFn, sortedArray } from "./utility"
 import data from "./data.json"
 export default function Gamma(){
-    let gammaData=addingKey(data,"Gamma")
+let gammaData=addingKey(data,"Gamma")
 let formatGamma=classWiseGroup(gammaData,"Alcohol")
 let valuesGamma=Object.values(formatGamma)
 let sortedArGamma=sortedArray(valuesGamma,"Gamma")
 
-console.log("SORTEDGAMMA",sortedArGamma.map(ar=>ar.map(dd=>dd.Gamma)))
 
     return    <div className="grid p20">
     <Header data={Object.keys(formatGamma)}/>
      <Mean data={ sortedArGamma.map((row)=>(row.reduce((pV,cI)=>pV+Number(cI.Gamma),0)/row.length).toFixed(3))} type={"Gamma"}/>
      <Median data={sortedArGamma.map((row)=>
       row.length%2===0?
-      ((row[row.length/2].Gamma+row[Number(row.length/2+1)].Gamma)/2).toFixed(3):(row[Number((row.length+1)/2)].Gamma).toFixed(3)
+      ((row[row.length/2-1].Gamma+row[Number(row.length/2)].Gamma)/2).toFixed(3):(row[Number((row.length-1)/2)].Gamma).toFixed(3)
      )}
      type={"Gamma"}
      />
